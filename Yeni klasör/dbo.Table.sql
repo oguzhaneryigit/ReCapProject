f890a9beaf369682fd0671cd6c4e29a8ddcce685
oguz erygt <oguzhanerygt@gmail.com>
2021-03-12 11:31:31 +0300

    11. ders 1. ödev

--- Yeni klasör/dbo.Table.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [BrandID] INT NULL, 
-    [ColorID] INT NULL, 
-    [ModelYear] NCHAR(6) NULL, 
-    [DailyPrice] MONEY NULL, 
-    [Description] NCHAR(20) NULL
-)
